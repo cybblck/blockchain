61484a0b99626d7aa8880d4330e0a5d1e2cd1f08
Shobhit Jain <104783891+bjshobhit@users.noreply.github.com>
2023-05-25 10:41:26 +0000

    About us section updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import meter1 from "../assets/img/polygon-logo-square-official.png";
+import meter2 from "../assets/img/hyperledger-logo-square-official.png";
+import meter3 from "../assets/img/ethereum-logo-square-black.png";
+import meter4 from "../assets/img/algorand_square_logo_black-official.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
@@ -36,27 +37,27 @@ export const Skills = () => {
                     <div className="skill-bx wow zoomIn">
                         <h2>About Us</h2>
                         <p>Behind the scenes at IBI. Explore the team, our values and much more</p>
-                        {/* <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Polygon</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Hyperledger</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>Ethereum</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <img src={meter4} alt="Image" />
+                                <h5>Algorand</h5>
                             </div>
-                        </Carousel> */}
-                        <h2>Learn. Execute. Master.</h2>
+                        </Carousel>
+                        {/* <h2>Learn. Execute. Master.</h2>
                         <p>Indian Blockchain Institute (IBI), the training division of Snapper Future Tech was founded with the primary goal of developing a productive and efficient Blockchain talent pool for the industry. With this goal in mind, IBI provides an entire ecosystem for upskilling in Blockchain through its range of training services for individuals, corporates, and academic institutions.
-Our training offerings include NASSCOM accredited programs, tailored corporate programs, setting up a Blockchain Centre of Excellence, and faculty development programmes for academic institutions. Our experts are proficient in a variety of blockchain frameworks, including Ethereum, Hyperledger, Algorand, and Polygon. We are one of the few authorised Service Providers and Training Partners of Hyperledger in the entire world.</p>
+Our training offerings include NASSCOM accredited programs, tailored corporate programs, setting up a Blockchain Centre of Excellence, and faculty development programmes for academic institutions. Our experts are proficient in a variety of blockchain frameworks, including Ethereum, Hyperledger, Algorand, and Polygon. We are one of the few authorised Service Providers and Training Partners of Hyperledger in the entire world.</p> */}
                     </div>
                 </div>
             </div>
