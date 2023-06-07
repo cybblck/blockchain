24afa7cf9a71b5e67cbe12e0806f2bc5d47b6df0
Shobhit Jain <104783891+bjshobhit@users.noreply.github.com>
2023-06-07 17:11:46 +0000

    team

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
+import { Team } from "./components/Team";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 
