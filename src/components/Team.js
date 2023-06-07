3a139ad9d910d98327b8e304683ac27fd46514ff
Shobhit Jain <104783891+bjshobhit@users.noreply.github.com>
2023-06-07 17:20:44 +0000

    team

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,113 +1,117 @@
-import meter1 from "../assets/img/polygon-logo-square-official.png";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-
-import meter2 from "../assets/img/hyperledger-logo-square-official.png";
-import meter3 from "../assets/img/ethereum-logo-square-black.png";
-import meter4 from "../assets/img/algorand_square_logo_black-official.png";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/event1.jpeg";
+import projImg2 from "../assets/img/event2.jpeg";
+import projImg3 from "../assets/img/event3.jpeg";
+import projImg4 from "../assets/img/event4.jpeg";
+import projImg5 from "../assets/img/event5.jpeg";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Team = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
+
+  const team = [
+    {
+      title: "Blockchain Introductory Program",
+      // description: "Design & Development",
+      imgUrl: projImg1,
     },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
+    {
+      title: "Certified Ethereum Developer Program",
+      // description: "Design & Development",
+      imgUrl: projImg2,
     },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
+    {
+      title: "Deep Dive Into Decentralized Finance(DEFI)",
+      // description: "Design & Development",
+      imgUrl: projImg3,
     },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
+    {
+      title: "Faculty Development Programs",
+      // description: "Design & Development",
+      imgUrl: projImg4,
+    },
+    {
+      title: "Blockchain Center of Excellence",
+      // description: "Design & Development",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Demo",
+      // description: "Design & Development",
+      imgUrl: projImg1,
+    },
+  ];
 
   return (
-    <section className="team" id="about">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Our Team</h2>
-                        <p>Trusted Partner</p>
+    <section className="project" id="blog">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Expertise</h2>
+                <p>Blockchain Trainings<br></br>
+Blockchain Development<br></br>
+Artificail Intellegence <br></br>
+IoT Development<br></br>
+Web Design<br></br>
+Web Development<br></br>
+Fintech<br></br>
+Supply Chain<br></br>
+</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Events</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Awards</Nav.Link>
 
+                    
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          teams.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>Conference on “Use of Smart Contract in Public Procurement and Supply Chain" on 17th-18th March 2023                       <a href="https://www.ajnifm.ac.in/event/conference-%E2%80%9Cuse-smart-contract-public-procurement-and-supply-chain-17th-18th-march-2023">see more</a>
+</p>
 
-
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>TIBA 	YARNA Labs	IIT Kanpur</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Tsinghua University Beijing 	</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Seth visambhar nath college </h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter4} alt="Image" />
-                                <h5>lucknow ABES ghaziabad </h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>5Udaan Skill Academy </h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>India Blockchain Alliance </h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>India Blockchain Alliance  </h5>
-                            </div>
-                        </Carousel>
-
-                      
-                        {/* <h2>Learn. Execute. Master.</h2>
-                        <p>Indian Blockchain Institute (IBI), the training division of Snapper Future Tech was founded with the primary goal of developing a productive and efficient Blockchain talent pool for the industry. With this goal in mind, IBI provides an entire ecosystem for upskilling in Blockchain through its range of training services for individuals, corporates, and academic institutions.
-Our training offerings include NASSCOM accredited programs, tailored corporate programs, setting up a Blockchain Centre of Excellence, and faculty development programmes for academic institutions. Our experts are proficient in a variety of blockchain frameworks, including Ethereum, Hyperledger, Algorand, and Polygon. We are one of the few authorised Service Providers and Training Partners of Hyperledger in the entire world.</p> */}
-                    <div className="why">
-                    <p>Leadership:-</p>
-
-                    <h6>
-Dr. Amit Kumar Vishwakarma <br></br>
-<a href=" https://www.linkedin.com/in/dr-amit-vishwakarma-0a4b79bb/	"> https://www.linkedin.com/in/dr-amit-vishwakarma-0a4b79bb</a>		<br></br>	
-Dr. Omji Pandey <br></br>
-<a href="linkedin.com/in/om-jee-pandey-b90054116/">linkedin.com/in/om-jee-pandey-b90054116</a>		<br></br>	
-Vijay Kumar Sharma<br></br>
-<a href="https://www.linkedin.com/in/vijaysharmacs/	">https://www.linkedin.com/in/vijaysharmacs</a>		<br></br>	
-</h6>
-</div>
-
-<div className="why">
-<h6>Why Us</h6>
-<p>We prioritize your growth</p>
-<h6>Our Vision</h6>
-<p>We believe in the work with quality and accuracy. We want to be recognized as the trusted, innovative, reliable, and user-friedly service provider in education sector</p>
-<h6>Our Mission</h6>
-
-
-<p>Your growth is our mission. Our mission is to technologically educate each and every indiciduals</p>
-</div>
-
-                    </div>
-
-
-                </div>
-            </div>
-        </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+<p>safeguarding dual use material and data using blockchain technology application.<br></br> <a href="https://www.crdfglobal.org/">see more</a></p>
+<p>AEPC Japan India Industry Promotion Association. <br></br><a href="https://www.aepcindia.com/node">see more</a></p>
+<p>Tsinghua Energy Hub<br></br>
+Tsinghua International Blockchain Association <br></br>
+<a href="https://www.linkedin.com/company/thu-tiba/about/">see more</a></p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   )
 }
